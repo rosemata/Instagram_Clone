dc604ca00b2031aad52508ee4daeb8176d4056e9
Roselle Mata <roselle.tmata@gmail.com>
2022-10-22 17:55:08 -0500

    oct 22 5:55 pm

---
+++ Instagram_Clone/PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  Instagram_Clone
+//
+//  Created by Roselle Mata on 10/22/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
